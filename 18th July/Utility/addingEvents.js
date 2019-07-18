87d86d591161862ca6169777bb1261e7e02891b0
shashank_deol <shashank.deol007@gmail.com>
2019-07-18 23:13:27 +0530

    Event Problem 2

--- 18th July/Utility/addingEvents.js
+++ 18th July/Utility/addingEvents.js
@@ -1,12 +1,10 @@
-/*
+
 var btn=document.getElementById("btn2");
 btn.addEventListener('click',multiply);
 //console.log(z);
-*/
-document.getElementById("myBtn").addEventListener("click", multiply);
 
 function multiply(){
     var txt1_value=document.getElementById("txt1").value;
     var txt2_value=document.getElementById("txt2").value;
-    console.log(txt1_value,txt2_value)  
+    console.log(txt1_value+txt2_value)  
 }
